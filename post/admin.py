655a0d0ef90bdf8ba3dd2e758045554529834ed1
Elena Hanna <hanna_elena@live.se>
2024-08-26 14:21:29 +0000

    Add models import to admin.py

--- post/admin.py
+++ post/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import Post, Comment, UserProfile, GameCategory
 
-# Register your models here.
+
+admin.site.register(UserProfile)
+admin.site.register(GameCategory)
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    """
+    Post model in admin panel
+    """
+    summernote_fields = ('content')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    """
+    Comment model in admin panel
+    """
+    list_display = ('user', 'body', 'post', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ('user__username', 'body')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        """
+        Approve user comments
+        """
+        queryset.update(approved=True)
