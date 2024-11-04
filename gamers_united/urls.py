1700639ad221cf55b5a37bcf4989d3ce2e0f0c3f
Elena Hanna <hanna_elena@live.se>
2024-11-04 09:10:23 +0000

    Update url and views py

--- gamers_united/urls.py
+++ gamers_united/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from gamers_united import views as index_views
 
 urlpatterns = [
+    path('', index_views.index, name='index'),
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
