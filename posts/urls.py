e55b048129c2b0242ef29f2fb7c5dd886686ecf2
Elena Hanna <hanna_elena@live.se>
2024-11-06 11:06:45 +0000

    Update view and urls for posts

---
+++ posts/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from .views import (
+    AddPost, Posts,
+    PostDetail, DeletePost,
+    EditPost
+)
+
+
+urlpatterns = [
+    path("add/", AddPost.as_view(), name="add_post"),
+    path("", Posts.as_view(), name="posts"),
+    path("<slug:pk>/", PostDetail.as_view(), name="post_detail"),
+    path("delete/<slug:pk>/", DeletePost.as_view(), name="delete_post"),
+    path("edit/<slug:pk>/", EditPost.as_view(), name="edit_post"),
+]
---
+++ posts/views.py
@@ -0,0 +1,67 @@
+from django.views.generic import (
+    CreateView, ListView,
+    DetailView, DeleteView,
+    UpdateView
+)
+
+from django.contrib.auth.mixins import (
+    UserPassesTestMixin, LoginRequiredMixin
+)
+
+from django.db.models import Q
+
+from .models import Post
+from .forms import PostForm
+
+
+class Posts(ListView):
+    template_name = "posts/posts.html"
+    model = Post
+    context_object_name = "posts"
+
+    def get_queryset(self, **kwargs):
+        query = self.request.GET.get('q')
+        if query:
+            posts = self.model.objects.filter(
+                Q(title__icontains=query) |
+                Q(content__icontains=query) |
+                Q(device__icontains=query)
+            )
+        else:
+            posts = self.model.objects.all()
+        return posts
+
+
+class PostDetail(DetailView):
+    template_name = "posts/post_detail.html"
+    model = Post
+    context_object_name = "post"
+
+
+class AddPost(LoginRequiredMixin, CreateView):
+    template_name = "posts/add_post.html"
+    model = Post
+    form_class = PostForm
+    success_url = "/posts/"
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super(AddPost, self).form_valid(form)
+
+
+class EditPost(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    template_name = 'posts/edit_post.html'
+    model = Post
+    form_class = PostForm
+    success_url = '/posts/'
+
+    def test_func(self):
+        return self.request.user == self.get_object().author
+
+
+class DeletePost(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/posts/'
+
+    def test_func(self):
+        return self.request.user == self.get_object().author
