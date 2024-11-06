1a9492071a61f5adb27595996c5d553e11de02a0
Elena Hanna <hanna_elena@live.se>
2024-11-06 11:00:13 +0000

    Update admin for posts

--- posts/admin.py
+++ posts/admin.py
@@ -1,25 +1,29 @@
 from django.contrib import admin
 from .models import Post, Comment, UserProfile, GameCategory
 
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'username', 'email', 'country')
+    search_fields = ('user__username', 'email', 'country')
 
-admin.site.register(UserProfile)
-admin.site.register(GameCategory)
+@admin.register(GameCategory)
+class GameCategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
 
-
-
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'status', 'created_on', 'category')
+    list_filter = ('status', 'created_on', 'category')
+    search_fields = ('title', 'author__username', 'category__name')
+    prepopulated_fields = {'slug': ('title',)}
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-    """
-    Comment model in admin panel
-    """
     list_display = ('user', 'body', 'post', 'created_on', 'approved')
     list_filter = ('approved', 'created_on')
     search_fields = ('user__username', 'body')
     actions = ['approve_comments']
 
     def approve_comments(self, request, queryset):
-        """
-        Approve user comments
-        """
         queryset.update(approved=True)
