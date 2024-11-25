a261c2d8fc8bca2c763136cc5a4d3f18d8ae1cdb
Elena Hanna <hanna_elena@live.se>
2024-11-25 10:12:48 +0000

    Debug

--- gamers_united/settings.py
+++ gamers_united/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = 'DEVELOPMENT' in os.environ
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '8000-enzolita-gamersunited-9sc8wk0a5l4.ws.codeinstitute-ide.net',
